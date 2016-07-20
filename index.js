21deb0866797fd5ad22f54719e4d0afb450a6de2
Dominic Watson <dominic.watson@cherrygroup.com>
2016-07-20 13:25:31 +0200

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var app = express();
 
+app.get('/hello-world', function(req, res) {
+    res.send('hello world');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on http://0.0.0.0:3000');
+});
