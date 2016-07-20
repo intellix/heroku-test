9b45c4b5aa74fa78d3b282babf47616f1c7dfccb
Dominic Watson <dominic.watson@cherrygroup.com>
2016-07-20 13:39:31 +0200

    Listen on 80

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/hello-world', function(req, res) {
     res.send('hello world');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on http://0.0.0.0:3000');
+app.listen(80, () => {
+  console.log('Listening on http://0.0.0.0:80');
 });
